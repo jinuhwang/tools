809a873d55086f54161422f045fe1c4ff0208de3
Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
2020-06-07 22:48:50 +0900

    Modify setup.sh to setup coc.nvim

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+if [ -d ~/bin ]; then
+    mkdir ~/bin
+fi
+
 # Install packages
-apt update && apt install -y --no-install-recommends \
+add-apt-repository ppa:jonathonf/vim
+apt update && apt install -y \
     build-essential \
     zsh \
     tmux
 
 # Download Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 # Copy dotfiles (Symbolic links are stale in the docker image, extract it to startup script later)
 cp $DIR/dotfiles/zshrc $HOME/.zshrc
@@ -15,9 +20,14 @@ cp $DIR/dotfiles/vimrc $HOME/.vimrc
 cp $DIR/dotfiles/tmux.conf $HOME/.tmux.conf
 cp $DIR/dotfiles/gitconfig $HOME/.gitconfig
 
+# Install Node for coc.nvim
+curl -sL install-node.now.sh/lts | bash
 
 # Install Vim Plugin
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install ccls for coc.nvim
+sh $DIR/vim/ccls.sh
--- vim/ccls.sh
+++ vim/ccls.sh
@@ -6,4 +6,5 @@ wget -c http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-
 tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
 cmake --build Release
+cp Release/ccls $HOME/bin/ccls
 # https://github.com/MaskRay/ccls/wiki/coc.nvim
