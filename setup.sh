76c840e8f35aa67042c049d143acf1a195f8a7d9
Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
2020-06-08 20:35:05 +0900

    Add setup script
    Squashed commit of the following:
    
    commit f73a62aa216cb755fbb02bca5d6b4f3359d07c75
    Author: Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
    Date:   Mon Jun 8 20:28:00 2020 +0900
    
        Set zsh as default shell for tmux
    
    commit 9b7ef7b37cc8f20b90ca24728062ece05ab02beb
    Author: Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
    Date:   Mon Jun 8 20:25:37 2020 +0900
    
        Skip user input for node installation
    
    commit 7b0e5d1095dc8a82d5bfae4134983d6c6bfa7823
    Author: Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
    Date:   Mon Jun 8 00:17:00 2020 +0900
    
        Fix setup to compile and install ccls
    
    commit 35b70997b842bf42a8e4f469b07db66bfb522c34
    Author: Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
    Date:   Sun Jun 7 23:02:42 2020 +0900
    
        Add minimal file for p10k
    
    commit 809a873d55086f54161422f045fe1c4ff0208de3
    Author: Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
    Date:   Sun Jun 7 22:48:50 2020 +0900
    
        Modify setup.sh to setup coc.nvim
    
    commit 7b337c5bbbc6e98bfd96f4ef63dfa0b2ded09263
    Author: Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
    Date:   Sun Jun 7 22:27:50 2020 +0900
    
        Rename dotfiles directory

--- setup.sh
+++ setup.sh
@@ -1,22 +1,40 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# TODO: Link binaries, install fzf
+if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+fi
+
+# To upgrade Vim
+add-apt-repository ppa:jonathonf/vim -y
+
 # Install packages
-apt update && apt install -y --no-install-recommends \
+apt update && apt install -y --upgrade \
     build-essential \
+    cmake \
     zsh \
-    tmux
+    tmux \
+    vim
+
+# Download Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 # Copy dotfiles (Symbolic links are stale in the docker image, extract it to startup script later)
 cp $DIR/dotfiles/zshrc $HOME/.zshrc
 cp $DIR/dotfiles/vimrc $HOME/.vimrc
 cp $DIR/dotfiles/tmux.conf $HOME/.tmux.conf
 cp $DIR/dotfiles/gitconfig $HOME/.gitconfig
+cp $DIR/dotfiles/p10k.zsh $HOME/.p10k.zsh
 
+# Install Node for coc.nvim
+curl -sL install-node.now.sh/lts | bash -s -- -y
 
 # Install Vim Plugin
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 
-# Setup Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install ccls for coc.nvim
+cd $DIR
+./vim/ccls.sh
--- vim/ccls.sh
+++ vim/ccls.sh
@@ -1,4 +1,4 @@
-sudo apt install zlib1g-dev libncurses-dev
+apt install zlib1g-dev libncurses-dev
 git clone --depth=1 --recursive https://github.com/MaskRay/ccls
 cd ccls
 
@@ -6,4 +6,5 @@ wget -c http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-
 tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
 cmake --build Release
+cp Release/ccls $HOME/bin/ccls
 # https://github.com/MaskRay/ccls/wiki/coc.nvim
