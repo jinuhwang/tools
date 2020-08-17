7180b84f482444872aa17a73faf52214569d275a
Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
2020-08-17 03:24:11 +0000

    Fix ccls to be properly installed with latest CMake

--- cmake/install-cmake.sh
+++ cmake/install-cmake.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 apt install -y libssl-dev
-curl -LO https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
-tar xvzf cmake-3.17.3.tar.gz
-cd cmake-3.17.3
+curl -LO https://github.com/Kitware/CMake/releases/download/v3.18.1/cmake-3.18.1.tar.gz
+tar xvzf cmake-3.18.1.tar.gz
+cd cmake-3.18.1
 ./configure
 make -j$(nproc)
 make install
-
+cd ..
--- vim/ccls.sh
+++ vim/ccls.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 apt install zlib1g-dev libncurses-dev
 git clone --depth=1 --recursive https://github.com/MaskRay/ccls
 cd ccls
-
+../cmake/install-cmake.sh
 wget -c http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04
