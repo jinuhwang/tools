073c10330929657be0149170d4885aa8951c5b2c
Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
2021-03-10 23:47:17 +0900

    Fix SMTP script

--- python/smtp.py
+++ python/smtp.py
@@ -14,7 +14,7 @@ def smtp_login(id, passwd):
     smtp.ehlo()
     smtp.starttls()
     code, ret = smtp.login(id, passwd)
-    print(ret.decode())
+    _print(ret.decode())
 
     if code >= 300:
         exit(1)
@@ -22,18 +22,13 @@ def smtp_login(id, passwd):
     return smtp
 
 
-def send_mail(send_to, send_from, subject, content):
+def send_mail(client, send_to, send_from, subject, content):
     msg = MIMEText(content)
     msg['Subject'] = subject
     msg['From'] = send_from
     msg['To'] = send_to
 
-    code, ret = smtp.sendmail(send_from, send_to, msg.as_string())
-    print(ret.decode())
-
-    if code >= 300:
-        exit(1)
-
+    client.sendmail(send_from, send_to, msg.as_string())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -47,25 +42,27 @@ if __name__ == '__main__':
     args = parser.parse_args()
     config = yaml.load(open(args.CONFIG))
     console = Console()
+    _print = console.print
 
     client = smtp_login(config['id'], config['passwd'])
 
     df = pd.read_csv(config['csv'])
-    df = df[[config['to_columns']] + config['columns']]
+    df = df[[config['to_columns']] + config['format_columns']]
 
-    for i, (to, *row) in track(df.iterrows(), description='Sending Email...'):
+    for i, (to, *row) in track(df.iterrows(), description='Sending Email...', total=len(df)):
         formatted = config['content'].format(*row)
 
-        if args.fire:
-            send_mail(to, config['from'], config['subject'], formatted)
-        elif args.test:
-            to = config['from']
-            send_mail(to, config['from'], config['subject'], formatted)
-        else:
-            print('Would send:')
-            print('From: ', config['from'])
-            print('to: ', to)
-            console.rule('[bold]' + config['subject'])
-            print(formatted)
+        _print()
+        _print('Would send:')
+        _print('From: ', config['from'])
+        _print('To: ', to)
+        _print('[bold]' + config['subject'])
+        _print(formatted)
 
         console.input('Press Enter to continue')
+
+        if args.fire:
+            send_mail(client, to, config['from'], config['subject'], formatted)
+        elif args.test:
+            to = config['from']
+            send_mail(client, to, config['from'], config['subject'], formatted)
