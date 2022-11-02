38b702b6c128b3af39d13d2e57380636d779b09e
Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
2022-11-02 00:26:35 +0000

    Remove deprecated function

--- dotfiles/config/nvim/lua/jinu/lsp.lua
+++ dotfiles/config/nvim/lua/jinu/lsp.lua
@@ -59,7 +59,7 @@ tabnine:setup({
 
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
 		on_attach = function()
 			Nnoremap("gd", ":lua vim.lsp.buf.definition()<CR>")
 			Nnoremap("gr", ":lua vim.lsp.buf.references()<CR>")
