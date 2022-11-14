bc2a622030f5279d0e09cc64dc0610d9b6430e3d
Jinwoo Hwang <jwhwang@casys.kaist.ac.kr>
2022-11-14 10:40:58 +0900

    Update setup from ubuntu-home

--- ubuntu/xrandr.sh
+++ ubuntu/xrandr.sh
@@ -1,4 +1,4 @@
-# xrandr --output DP-0  --auto --output DP-2 --auto --panning 1354x2408+0+0 --rotate left --scale 0.627x0.627 
+# xrandr --output DP-0  --auto --output DP-2 --auto --panning 1354x2408+0+0 --rotate left --scale 0.627x0.627
 # xrandr \
 #     --output DP-0  --primary --pos 1354x0 --mode 3840x2160 --fb 5194x2408 --panning 0x0+0+0 \
 #     --output DP-2  --rotate left --scale 0.627x0.627 --pos 0x0 --panning 0x0+0+0 --fb 5194x2408
@@ -14,9 +14,6 @@
 #     --output DP-0 --auto --pos 1354x0 --primary
 
 xrandr \
-    --output DP-0 --off \
-    --output DP-2 --noprimary --auto --scale 0.63x0.63 --rotate left  --panning 1361x2419+0+0 \
-    --fb 5201x2419
-
-xrandr \
-    --output DP-0 --primary --scale 0.75x0.75 --mode 5120x2160 --pos 1361x390 --panning 3840x1620+1361+390
+    --output HDMI-0 --auto --noprimary --scale 1.184x1.184 --pos 0x317 --panning 2273x1279+0+317 \
+    --output DP-2 --auto --primary --pos 2273x317 --panning 3840x1600+2274+317 \
+    --output DP-4 --auto --noprimary --scale 1.164x1.164 --rotate right --pos 6114x0
